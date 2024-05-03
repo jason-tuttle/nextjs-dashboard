758a3ae9c315969f5b18682fc589e345acb7c732
Jason Tuttle <jason.tuttle@greenlight.guru>
2024-05-03 15:07:59 -0400

    complete chapter 14

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -6,18 +6,18 @@ export default async function Page() {
   const customers = await fetchCustomers();
 
   return (
-	<main>
-	  <Breadcrumbs
-		breadcrumbs={[
-		  { label: 'Invoices', href: '/dashboard/invoices' },
-		  {
-			label: 'Create Invoice',
-			href: '/dashboard/invoices/create',
-			active: true,
-		  },
-		]}
-	  />
-	  <Form customers={customers} />
-	</main>
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Create Invoice',
+            href: '/dashboard/invoices/create',
+            active: true,
+          },
+        ]}
+      />
+      <Form customers={customers} />
+    </main>
   );
 }
